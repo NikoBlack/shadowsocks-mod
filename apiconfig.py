8750f23569835c037ab03b9251290ed03cd2cdbf
NikoBlack <45842900+NikoBlack@users.noreply.github.com>
2020-07-02 19:55:15 +0800

    Update apiconfig.py

--- apiconfig.py
+++ apiconfig.py
@@ -1,54 +1,18 @@
 # Config
-NODE_ID = 0
+NODE_ID = 7
 
-# hour,set 0 to disable
-SPEEDTEST = 6
-CLOUDSAFE = 1
-ANTISSATTACK = 0
-AUTOEXEC = 0
 
-MU_SUFFIX = 'zhaoj.in'
-MU_REGEX = '%5m%id.%suffix'
+# Mode
+API_INTERFACE = 'modwebapi'         # glzjinmod, modwebapi
 
-SERVER_PUB_ADDR = '127.0.0.1'  # mujson_mgr need this to generate ssr link
-API_INTERFACE = 'modwebapi'  # glzjinmod, modwebapi
+WEBAPI_URL = 'https://xmucloud.com'
+WEBAPI_TOKEN = 'key-net999$xmu+-%~.;ytf@*ytv76'
 
-WEBAPI_URL = 'https://zhaoj.in'
-WEBAPI_TOKEN = 'glzjin'
 
-# mudb
-MUDB_FILE = 'mudb.json'
+# onport for multiple users, set also in SS-PANEL - .config.php
+MU_SUFFIX = 'microsoft.com'
+MU_REGEX = '%8m%id.%suffix'
 
-# Mysql
-MYSQL_HOST = '127.0.0.1'
-MYSQL_PORT = 3306
-MYSQL_USER = 'ss'
-MYSQL_PASS = 'ss'
-MYSQL_DB = 'shadowsocks'
-
-MYSQL_SSL_ENABLE = 0
-MYSQL_SSL_CA = ''
-MYSQL_SSL_CERT = ''
-MYSQL_SSL_KEY = ''
-
-# API
-API_HOST = '127.0.0.1'
-API_PORT = 80
-API_PATH = '/mu/v2/'
-API_TOKEN = 'abcdef'
-API_UPDATE_TIME = 60
-
-# Manager (ignore this)
-MANAGE_PASS = 'ss233333333'
-# if you want manage in other server you should set this value to global ip
-MANAGE_BIND_IP = '127.0.0.1'
-# make sure this port is idle
-MANAGE_PORT = 23333
-
-# edit this file and server will auto reload
-
-# boolean, enable to print mysql query
-PRINT_MYSQL_QUERY = False
 
 # second
 MYSQL_PUSH_DURATION = 60
@@ -58,4 +22,52 @@ get port offset by node->name
 HK 1 #9900
 then offset is 9900
 """
-GET_PORT_OFFSET_BY_NODE_NAME = True
+GET_PORT_OFFSET_BY_NODE_NAME = False     # True | False
+
+
+
+########################### Detail Config ###############################
+
+
+# Auto speedtest-cli, 0 to disable, hours, set also in SS-PANEL
+SPEEDTEST = 0
+CLOUDSAFE = 1
+ANTISSATTACK = 0
+AUTOEXEC = 0
+
+# SSR link generation
+SERVER_PUB_ADDR = '129.x.x.32/url'  # mujson_mgr need this to generate ssr link
+
+# mudb
+MUDB_FILE = 'mudb.json'
+
+# Mysql
+MYSQL_HOST = '127.0.0.1'
+MYSQL_PORT = 3306
+MYSQL_USER = 'username@#%FEW^%*3_+15'
+MYSQL_PASS = 'passwordEVG32#@*(%$_+5'
+MYSQL_DB   = 'dbnameCCY$%613~!$%^&(T'
+
+MYSQL_SSL_ENABLE = 0
+MYSQL_SSL_CA = ''
+MYSQL_SSL_CERT = ''
+MYSQL_SSL_KEY = ''
+
+# API
+API_HOST = '127.0.0.1'
+API_PORT = 58568
+API_PATH = '/doe3-451sn-ot4-5exist-SJd324s43-5c-DCdscVF-33244-WJd34-5cFDdc-s9sg43-5FDgf-dc248905D-CW'
+API_TOKEN = 'hcf-=ie-=w8327K~.[JHD-=C.[.;]IW8`1~!@#~723#@$-=2,/<>m#%!%-=:{:$&-=%*&_+_+_))_#@$%-=@!$#'
+API_UPDATE_TIME = 36000000
+
+# Manager (ignore this)
+MANAGE_PASS = 'KDS-={324>><:@#$1~!`{ds124a-`#@=-#24$@%!$><?\%s`a98--=32sdavJ#@%^3242\KEAS-=d-s~#@~a321JHf`ds2-=143YUd#\^%$#`s43215-=WQNC'
+# if you want manage in other server you should set this value to global ip
+MANAGE_BIND_IP = '127.0.0.1'
+# make sure this port is idle
+MANAGE_PORT = 58569
+
+# boolean, enable to print mysql query
+PRINT_MYSQL_QUERY = False
+
+
