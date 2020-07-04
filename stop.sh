f7e109688609e54b54205943faac1ddc60328157
NikoBlack <45842900+NikoBlack@users.noreply.github.com>
2020-07-04 20:30:29 +0800

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 python_ver=$(ls /usr/bin|grep -e "^python[23]\.[1-9]\+$"|tail -1)
-eval $(ps -ef | grep "[0-9] ${python_ver}  server\\.py m" | awk '{print "kill -9 "$2}')
+eval $(ps -ef | grep "[0-9] ${python_ver} server\\.py m" | awk '{print "kill -9 "$2}')
